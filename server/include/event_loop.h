531fb202183522dfb4cab47204d925a3221fe9bf
elysium-w <wya10241110@163.com>
2024-02-12 01:34:24 +0800

    feat(server):引入计时器队列调度

--- server/include/event_loop.h
+++ server/include/event_loop.h
@@ -3,6 +3,8 @@
 #define DTIMEDB_EVENT_LOOP_H
 #include <hash_set>
 #include <hash_map>
+#include "event_base.h"
+
 #define MAX_EVENTS 10
 class event_loop{
 public:
@@ -11,6 +13,8 @@ public:
 private:
     int  _epfd;
     struct epoll_event _fired_evs[MAX_EVENTS];
-    hash_map<int ,io_event>
+    __gnu_cxx::hash_map<int ,io_event> _io_evs;
+    typedef __gnu_cxx::hash_map<int ,io_event>::iterator io_ev_it;
+
 };
 #endif //DTIMEDB_EVENT_LOOP_H
---
+++ server/include/timer_queue.h
@@ -0,0 +1,14 @@
+//
+// Created by illumwang on 24-2-11.
+//
+
+#ifndef DTIMEDB_TIMER_QUEUE_H
+#define DTIMEDB_TIMER_QUEUE_H
+class timer_queue{
+public:
+
+private:
+    int _time_count;
+
+};
+#endif //DTIMEDB_TIMER_QUEUE_H
